61f18814bde175efaf9f54bab26b2873a23ed787
nicolas naas <nicolas_naas@outlook.fr>
2022-11-22 12:57:04 +0100

    Mthodes animal controller + views (pas fini)

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,14 +1,20 @@
 class AnimalsController < ApplicationController
   def index
+   @animals = Animal.all
   end
 
   def show
+    @animal = Animal.find(params[:id])
   end
 
   def new
+    @animal = Animal.new
   end
 
   def create
+    @animal = Animal.new(animal_params)
+    @animal.save
+    redirect_to animal_path(@animal) 
   end
 
   def edit
@@ -18,5 +24,9 @@ class AnimalsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def animal_params
+    params.require(:animal).permit(:name, :category, :description, :fear, :price)
   end
 end
