8b25d6236d550bff46859a056a66d211dfccf129
Amala Harrat <amal.harrat@hotmail.fr>
2022-11-24 08:57:35 +0100

    edit debug DONE + CARD zoom update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "animals#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :animals, exept:[:index] do
+  resources :animals, exept: [:index] do
     resources :bookings, only: %i[index new create edit update]
   end
   resources :bookings, only: :destroy
