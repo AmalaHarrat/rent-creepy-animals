2340af81509a8fbd7db4ce4f2626cc9b9a9fc2a2
saliha laidi <saliha.laidi@hotmail.com>
2022-11-24 18:32:19 +0100

    status en cours

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_093748) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_162101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_093748) do
     t.datetime "updated_at", null: false
     t.bigint "animal_id", null: false
     t.bigint "user_id", null: false
+    t.integer "status", default: 0
     t.index ["animal_id"], name: "index_bookings_on_animal_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
